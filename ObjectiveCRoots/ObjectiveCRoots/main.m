277c1fa2d1d650bb14e7306fc6fcd0df2936f1e4
kedar <kedar.desai@in.v2solutions.com>
2014-04-09 13:55:40 +0530

    Added the learnings obtained from this sample.

--- ObjectiveCRoots/ObjectiveCRoots/main.m
+++ ObjectiveCRoots/ObjectiveCRoots/main.m
@@ -20,3 +20,6 @@ int main(int argc, const char * argv[])
     return 0;
 }
 
+
+// Learning 1: We cannot use forward decleration @class Greeting over here instead of #import "Greeting.h" because here we are also calling the methods of the Greeting class. Forward declearation can be done only in situations where only the class instance is to be referenced. If methods are also to be accessed then we would have to do an import.
+
